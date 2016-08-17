087ccb2bb77e252bdf5ab54de4028586981904d6
olganagorna <olechka.nagorna@gmail.com>
2016-08-17 17:00:37 +0300

    js logic

--- controllers/recipes_controller.js
+++ controllers/recipes_controller.js
@@ -8,28 +8,25 @@ function RecipesCtrl($scope) {
 	$scope.recipes = (localStorage.getItem('recipes')!==null) ? JSON.parse($scope.saved) : [];
 	localStorage.setItem('recipes', JSON.stringify($scope.recipes));
     $scope.model = [];
-    $scope.edit_model = [];
+    // $scope.edit_model = [];
     // console.log($scope.recipes[0].versions[0].title);
     // $scope.edit_model.title = $scope.recipes[0].versions[0].title;
-    for(var i = 0; i < $scope.recipes.length; i++){
-    	for(var k = 0; k < $scope.recipes[i].versions.length; k++){
-    		$scope.edit_model.title = $scope.recipes[i].versions[k].title;
-    	}
-    }
+
+    
 
 
 
-	$scope.addRecipe = function() {
+	$scope.addRecipe = function(index) {
 		$scope.recipes.push({
 			versions : [
 				{	
-					title: $scope.model.title,
-					description: $scope.model.description,
+					title: $scope.model[index].title,
+					description: $scope.model[index].description,
 					createTime: new Date()
 				}
 			]
 		});
-		$scope.model = '';
+		$scope.model = [];
 		localStorage.setItem('recipes', JSON.stringify($scope.recipes));
 	};
 	
@@ -41,13 +38,28 @@ function RecipesCtrl($scope) {
 	$scope.select= function(index) {
        $scope.active = index;
     };
+
 	$scope.editRecipe = function(index){
 		$scope.recipes[index].versions.push({
-			title: $scope.edit_model.title,
-			description: $scope.edit_model.description,
+			title: $scope.model[index].edit_model[0].title,
+			description: $scope.model[index].edit_model[0].description,
 			createTime: new Date()
 		});
 		localStorage.setItem('recipes', JSON.stringify($scope.recipes));
+		console.log($scope.recipes);
 	};
-
+	$scope.edit = function(index){
+		for(var i = 0; i < $scope.recipes.length; i++){
+			$scope.model.push({
+				edit_model: []
+			});
+			$scope.last_version = $scope.recipes[i].versions.length - 1;
+			for(var k = 0; k < $scope.model.length; k++){
+				$scope.model[k].edit_model.push({
+					title: $scope.recipes[i].versions[$scope.last_version].title,
+					description: $scope.recipes[i].versions[$scope.last_version].description,
+				});
+	    	}
+	    }
+	};
 }
